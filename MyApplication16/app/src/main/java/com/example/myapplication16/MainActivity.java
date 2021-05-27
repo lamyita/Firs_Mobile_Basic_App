acea973678db092ca57bf389d2926b7349a09004
lamyita <50775487+lamyita@users.noreply.github.com>
2021-05-27 09:40:50 +0100

    first test app mobile

---
+++ MyApplication16/app/src/main/java/com/example/myapplication16/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication16;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapplication16.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
